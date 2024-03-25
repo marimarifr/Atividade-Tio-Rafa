c66dcec290df869c29c25838e76de59bd591e6e2
MARIANA FERNANDES ROCHA <B51099@fgv.edu.br>
2024-03-25 08:03:26 -0300

    Atividade do absoluto para cpp

--- atividade.cpp
+++ atividade.cpp
@@ -4,7 +4,29 @@ using std::cout;
 using std::endl;
 using std::string;
 
+int absolute(int);
+
 int main()
 {
-    return 0
+    absolute(0);
+
+
+    return 0;
+}
+
+int absolute(int numero)
+{
+    if (numero >= 0)
+    {
+        cout << "O absoluto do número é: " << numero << endl; 
+    }
+
+    else if (numero < 0)
+    {
+        numero = numero*(-1);
+        cout << "O absoluto do número é: " << numero << endl;
+    };
+
+    return numero;
+
 }
